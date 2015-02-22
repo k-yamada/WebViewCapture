b7cfb5693beffffd4b11b614ccc2d6ce3f97f67c
k-yamada <yamadakazu45@gmail.com>
2015-02-22 19:29:53 +0900

    Initial Commit

---
+++ WebViewCapture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewCapture
+//
+//  Created by 山田 和弘 on 2015/02/22.
+//  Copyright (c) 2015年 k-yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
